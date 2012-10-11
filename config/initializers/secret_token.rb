b44c6a6f56df407d3521e77433d8c1c7820b777c
Gemma Pou <gemma@trencabits.com>
2012-10-11 18:25:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nina::Application.config.secret_token = '78e46dfb16a86b6ea7aee62ba6dda5ca5e22cb7bb8ad470119b4497d42cb044883e23423df2bc9a176416cf9ee916ba535df711f5fc13667cad98cda4d2210e4'
