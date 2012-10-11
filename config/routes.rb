4e4e18997598fe285fdc389cbb00436c39b3d1b3
Gemma Pou <gemma@trencabits.com>
2012-10-11 20:04:20 +0200

    Create the static pages views, helper, and general layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Nina::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/quisom"
-  get "static_pages/contacte"
+
+  root to: 'static_pages#home'
+
+  match '/quisom',   to: 'static_pages#quisom'
+  match '/contacte', to: 'static_pages#contacte'
 
 end
