9fbc50e0e0354cf1eba40037b96101231460f5ea
Gemma Pou <gemma@trencabits.com>
2012-10-13 13:37:49 +0200

    I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
   protect_from_forgery
   include SessionsHelper
 
@@ -12,4 +13,11 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def default_url_options(options={})
+      { :locale => I18n.locale }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Nina::Application.routes.draw do
 
-  resources :products
+  match '/:locale' => 'static_pages#home'
+  
+  scope "(:locale)", :locale => /ca|es|en/ do
+    root to: 'static_pages#home'
+    resources :products
 
-  root to: 'static_pages#home'
+    match '/quisom',   to: 'static_pages#quisom'
+    match '/contacte', to: 'static_pages#contacte'
 
-  match '/quisom',   to: 'static_pages#quisom'
-  match '/contacte', to: 'static_pages#contacte'
+    resources :sessions, only: [:new, :create, :destroy]
+    match '/signin',  to: 'sessions#new'
+    match '/signout', to: 'sessions#destroy', via: :delete
+  end
 
-  resources :sessions, only: [:new, :create, :destroy]
-  match '/signin',  to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy', via: :delete
 end
